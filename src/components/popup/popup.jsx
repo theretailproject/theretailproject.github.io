d78536c95c633c440c3e3f5bce35262517011ec6
Tanisha Rao <tanisharao810@gmail.com>
2025-06-26 02:36:54 +0530

    Shop Update

--- src/components/popup/popup.jsx
+++ src/components/popup/popup.jsx
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from "react";
 import Particles from "react-tsparticles";
-import { loadConfettiPreset } from "tsparticles-preset-confetti"; 
+import { loadConfettiPreset } from "tsparticles-preset-confetti";
 import "./popup.scss";
 
 const Popup = ({ message = "Product added!", onClose }) => {
   const [show, setShow] = useState(true);
 
   const particlesInit = async (engine) => {
-    await loadConfettiPreset(engine); 
+    await loadConfettiPreset(engine);
   };
 
   useEffect(() => {
@@ -27,13 +27,22 @@ const Popup = ({ message = "Product added!", onClose }) => {
         init={particlesInit}
         options={{
           preset: "confetti",
-          fullScreen: { enable: true }, // stays in the popup
+          fullScreen: { enable: true, zIndex: 99 },
         }}
       />
       <div className="popup-box">
+        <img
+          src={
+            message === "Product added to Wishlist!"
+              ? require("../shop/liked.png")
+              : message === "Product added to Cart!" ? require("../shop/cartFilled.png"):null
+          }
+          className="image-background"
+        />
         <span className="popup-close" onClick={() => setShow(false)}>
           &times;
         </span>
+
         <p className="popup-text">{message}</p>
       </div>
     </div>
--- src/components/shop/shop.jsx
+++ src/components/shop/shop.jsx
@@ -9,17 +9,104 @@ import { Play } from "./play";
 import { useCollectionData } from "react-firebase-hooks/firestore";
 import Preserve from "./preserve";
 import { AllProducts } from "./allproducts";
-
+import { useState } from "react";
 function Shop() {
   const location = useLocation();
 
   const isActive = (path) => location.pathname === path;
 
+  const wearCategories = [
+    {
+      name: "Bandana",
+      subcategories: [
+        "Silk Bandana"
+      ],
+    },
+    {
+      name: "Shirt",
+      subcategories: [
+        "Collared Shirt"
+      ],
+    },
+    {
+      name: "Collar",
+      subcategories: [
+        "Bib Collar",
+        "Scrunchie Collar",
+        "Frill Collar with Net"
+      ],
+    },
+    {
+      name: "Frock",
+      subcategories: [
+        "Festive Frock",
+      ],
+    },
+    {
+      name: "Festive",
+      subcategories: ["Sherwani", "Lehenga", "Kurta", "Ghagra", "Frock","Jacket/ Coat"],
+    },
+     {
+      name: "Scrunchie",
+      subcategories: ["Classic", "Long Tail", "Small Tail"],
+    },
+     {
+      name: "Bow",
+      subcategories: ["Stuffed", "Sailor"],
+    },
+  ];
+
+  const sleepCategories = [
+    {
+      name: "Beds",
+      subcategories: ["Dough Nut Bed", "Patch Work Bed"],
+    },
+    {
+      name: "Pet Tent",
+      subcategories: [],
+    },
+    {
+      name: "Snuffle Mat",
+      subcategories: ["Soft Mat", "Hard Mat"],
+    },
+  ];
+
+  const walkCategories = [
+    {
+      name: "Harness + Leash Combo",
+      subcategories: ["Matching Set"],
+    },
+     {
+      name: "Poop Bag",
+      subcategories: [],
+    },
+  ];
+
+  const playCategories = [
+    {
+      name: "Rope Toys",
+      subcategories: [ "Tug Toy"],
+    },
+    {
+      name: "Teether Toys",
+      subcategories: ["Fish Teether", "Bone Teether"],
+    },
+    {
+      name: "Snuffle Toys",
+      subcategories: ["Ball"],
+    },
+    {
+      name: "Chrochet Toys",
+      subcategories: ["Orange", "Bird", "Carrot", "Ball","Banana", "Mango", "Moon", "Heart", "Strawberry", "Ice Cream", "Soft Bone", "Honey Bee"],
+    },
+  ];
+
+  const [hoveredCategory, setHoveredCategory] = useState(null);
   return (
     <div className="Shop">
       <div className="shop-box">
         <div className="shop-cats-box">
-        <Link to="/shop">
+          <Link to="/shop">
             <p
               className={
                 isActive("/shop") || isActive("/shop")
@@ -30,50 +117,145 @@ function Shop() {
               All Products
             </p>
           </Link>
-          <Link to="/shop/wear">
-            <p
-              className={
-                isActive("/shop/wear")
-                  ? "shop-cat-name activeCat"
-                  : "shop-cat-name"
-              }
-            >
-              Wear
-            </p>
-          </Link>
-          <Link to="/shop/walk">
-            <p
-              className={
-                isActive("/shop/walk")
-                  ? "shop-cat-name activeCat"
-                  : "shop-cat-name"
-              }
-            >
-              Walk
-            </p>
-          </Link>
-          <Link to="/shop/play">
-            <p
-              className={
-                isActive("/shop/play")
-                  ? "shop-cat-name activeCat"
-                  : "shop-cat-name"
-              }
-            >
-              Play
-            </p>
-          </Link>
-          <Link to="/shop/sleep">
-            <p
-              className={
-                isActive("/shop/sleep")
-                  ? "shop-cat-name activeCat"
-                  : "shop-cat-name"
-              }
-            >
-              Sleep
-            </p>
-          </Link>
+          <div className="dropdown-wrapper">
+            <Link to="/shop/wear">
+              <p className="shop-cat-name">Wear ▼</p>
+            </Link>
+
+            <div className="dropdown-content">
+              <div className="dropdown-content-col">
+                <div className="dropdown-content-col1">
+                  {wearCategories.map((category, index) => (
+                    <p
+                      key={index}
+                      className="dropdown-heading"
+                      onMouseEnter={() => setHoveredCategory(category)}
+                    >
+                      {category.name}
+                    </p>
+                  ))}
+                </div>
+
+                <div className="dropdown-content-col2">
+                  {hoveredCategory?.subcategories.map((sub, subIndex) => (
+                    <Link
+                      key={subIndex}
+                      to={`/shop/wear/${hoveredCategory.name.toLowerCase()}/${sub
+                        .toLowerCase()
+                        .replace(/\s+/g, "-")}`}
+                    >
+                      {sub}
+                    </Link>
+                  ))}
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="dropdown-wrapper">
+            <Link to="/shop/sleep">
+              <p className="shop-cat-name">Sleep ▼</p>
+            </Link>
+
+            <div className="dropdown-content">
+              <div className="dropdown-content-col">
+                <div className="dropdown-content-col1">
+                  {sleepCategories.map((category, index) => (
+                    <p
+                      key={index}
+                      className="dropdown-heading"
+                      onMouseEnter={() => setHoveredCategory(category)}
+                    >
+                      {category.name}
+                    </p>
+                  ))}
+                </div>
+
+                <div className="dropdown-content-col2">
+                  {hoveredCategory?.subcategories.map((sub, subIndex) => (
+                    <Link
+                      key={subIndex}
+                      to={`/shop/wear/${hoveredCategory.name.toLowerCase()}/${sub
+                        .toLowerCase()
+                        .replace(/\s+/g, "-")}`}
+                    >
+                      {sub}
+                    </Link>
+                  ))}
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="dropdown-wrapper">
+            <Link to="/shop/walk">
+              <p className="shop-cat-name">Walk ▼</p>
+            </Link>
+
+            <div className="dropdown-content">
+              <div className="dropdown-content-col">
+                <div className="dropdown-content-col1">
+                  {walkCategories.map((category, index) => (
+                    <p
+                      key={index}
+                      className="dropdown-heading"
+                      onMouseEnter={() => setHoveredCategory(category)}
+                    >
+                      {category.name}
+                    </p>
+                  ))}
+                </div>
+
+                <div className="dropdown-content-col2">
+                  {hoveredCategory?.subcategories.map((sub, subIndex) => (
+                    <Link
+                      key={subIndex}
+                      to={`/shop/wear/${hoveredCategory.name.toLowerCase()}/${sub
+                        .toLowerCase()
+                        .replace(/\s+/g, "-")}`}
+                    >
+                      {sub}
+                    </Link>
+                  ))}
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="dropdown-wrapper">
+            <Link to="/shop/play">
+              <p className="shop-cat-name">Play ▼</p>
+            </Link>
+
+            <div className="dropdown-content">
+              <div className="dropdown-content-col">
+                <div className="dropdown-content-col1">
+                  {playCategories.map((category, index) => (
+                    <p
+                      key={index}
+                      className="dropdown-heading"
+                      onMouseEnter={() => setHoveredCategory(category)}
+                    >
+                      {category.name}
+                    </p>
+                  ))}
+                </div>
+
+                <div className="dropdown-content-col2">
+                  {hoveredCategory?.subcategories.map((sub, subIndex) => (
+                    <Link
+                      key={subIndex}
+                      to={`/shop/wear/${hoveredCategory.name.toLowerCase()}/${sub
+                        .toLowerCase()
+                        .replace(/\s+/g, "-")}`}
+                    >
+                      {sub}
+                    </Link>
+                  ))}
+                </div>
+              </div>
+            </div>
+          </div>
 
           <Link to="/shop/preserve">
             <p
@@ -89,7 +271,7 @@ function Shop() {
         </div>
 
         <Routes>
-        <Route path="/" element={<AllProducts />} />
+          <Route path="/" element={<AllProducts />} />
           <Route path="/wear" element={<Wear />} />
           <Route path="/walk" element={<Walk />} />
           <Route path="/play" element={<Play />} />
